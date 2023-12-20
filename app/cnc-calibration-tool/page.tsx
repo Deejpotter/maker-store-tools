89c3ceb83d554cfce833dac3ed41d0ec5b1c4d0e
Deej Potter <deejpotter@gmail.com>
2023-12-21 07:46:38 +1100

    Updated calibration tool.

--- app/cnc-calibration-tool/page.tsx
+++ app/cnc-calibration-tool/page.tsx
@@ -1,13 +1,79 @@
-"use client";
-import LayoutContainer from "@/components/LayoutContainer";
 import React, { useState, useEffect } from "react";
+import LayoutContainer from "@/components/LayoutContainer";
 
-const CndCalibrationTool: React.FC = () => {
-  return (
-    <LayoutContainer>
-      <h1>CNC Calibration Tool</h1>
-    </LayoutContainer>
-  );
+/**
+ * Functional component for CNC Calibration Tool.
+ * It calculates the new steps per millimeter based on user inputs.
+ */
+const CncCalibrationTool: React.FC = () => {
+	// State for current steps per millimeter
+	const [currentSteps, setCurrentSteps] = useState(0);
+	// State for expected movement in millimeters
+	const [expectedMovement, setExpectedMovement] = useState(0);
+	// State for actual movement in millimeters
+	const [actualMovement, setActualMovement] = useState(0);
+	// State for the new calculated steps per millimeter
+	const [newSteps, setNewSteps] = useState(0);
+
+	/**
+	 * Calculates the new steps per millimeter.
+	 */
+	const calculateNewSteps = () => {
+		if (actualMovement === 0) {
+			return 0; // Prevent division by zero
+		}
+		return (currentSteps * expectedMovement) / actualMovement;
+	};
+
+	// Effect hook to update the new steps per millimeter whenever inputs change
+	useEffect(() => {
+		setNewSteps(calculateNewSteps());
+	}, [currentSteps, expectedMovement, actualMovement]);
+
+	return (
+		<LayoutContainer>
+			<h1>CNC Calibration Tool</h1>
+			<div>
+				<label>
+					Current Steps/mm:
+					<input
+						type="number"
+						value={currentSteps}
+						onChange={(e) => setCurrentSteps(Number(e.target.value))}
+						min="0"
+					/>
+				</label>
+			</div>
+			<div>
+				<label>
+					Expected Movement (mm):
+					<input
+						type="number"
+						value={expectedMovement}
+						onChange={(e) => setExpectedMovement(Number(e.target.value))}
+						min="0"
+					/>
+				</label>
+			</div>
+			<div>
+				<label>
+					Actual Movement (mm):
+					<input
+						type="number"
+						value={actualMovement}
+						onChange={(e) => setActualMovement(Number(e.target.value))}
+						min="0"
+					/>
+				</label>
+			</div>
+			<div>
+				<label>
+					New Steps/mm:
+					<input type="number" value={newSteps.toFixed(2)} readOnly />
+				</label>
+			</div>
+		</LayoutContainer>
+	);
 };
 
-export default CndCalibrationTool;
+export default CncCalibrationTool;
